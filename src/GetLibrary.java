8b361f177cbbfd8124689a42992f6ea60b176d35
Vadym Miakishev <barrrssss@gmail.com>
2020-07-11 22:56:27 +0300

    add database connection to servlet

--- src/GetLibrary.java
+++ src/GetLibrary.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.sql.*;
 
 public class GetLibrary extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
@@ -6,6 +7,31 @@ public class GetLibrary extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        final String connectionUrl = "jdbc:mysql://localhost:3306/library";
+        final String user = "root";
+        final String password = "root";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl, user, password);
+             Statement statement = connection.createStatement()){
+            ResultSet resultSet = statement.executeQuery("Select * from books");
+
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                String author = resultSet.getString(3);
+                int quantity = resultSet.getInt(4);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
